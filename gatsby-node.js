a7109b46f75c5b131e040685d45e7a25bfbe9256
James Asbery <james.a@mmtdigital.co.uk>
2018-07-30 11:43:50 +0100

    init

---
+++ gatsby-node.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allContentfulPage {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      console.log(result.errors)
+      Promise.reject(result.errors)
+    }
+
+    const pages = result.data.allContentfulPage.edges
+
+    pages.forEach((page) => {
+      const { slug } = page.node
+
+      createPage({
+        path: `/blog/${slug}/`,
+        component: path.resolve(`./src/templates/page.js`),
+        context: {
+          slug: slug
+        },
+      })
+    })
+  })
+}
