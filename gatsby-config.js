26a8fd2715286e3896314eaa531f3f3c550b85fc
James Asbery <james.a@mmtdigital.co.uk>
2018-08-09 08:59:42 +0100

    Changes to env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
@@ -9,17 +7,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        environment: process.env.CONTENTFUL_ENVIRONMENT
-      }
-    },
-    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+      }
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID || `i1ri1xca7nyi`,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN || `9a393f9a9c5d6901cf8df1642e23654f5a3f10c469e6e1cd9cfe8583a2fc5c52`,
+        host: process.env.GATSBY_CONTENTFUL_HOST || undefined
       }
     }
   ]
